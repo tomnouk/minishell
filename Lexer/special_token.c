0ce4bad7b8b070cd69fc976d9f1eb21c83db29b2
Amir Eid <aeid@c2r3p11.42firenze.it>
2024-06-13 19:01:15 +0200

    test merge fail

--- Lexer/special_token.c
+++ Lexer/special_token.c
@@ -6,7 +6,7 @@
 /*   By: aeid <aeid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 17:38:30 by aeid              #+#    #+#             */
-/*   Updated: 2024/06/13 16:48:42 by aeid             ###   ########.fr       */
+/*   Updated: 2024/06/13 19:01:04 by aeid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void ft_special_token(t_data *data, t_types type)
 {
+	//ciao test
+	/*
 	
+	asdfdsafdsa
+	dsfasdfa
+	sdfasd
+	fasdfads
+	fads
+	fdsafas
+	dfasasdfsd
+	
+	
+	*/
 }
