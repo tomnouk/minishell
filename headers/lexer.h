e22938b25a4884810498923fe0248850492cf47d
Amir Eid <aeid@c2r3p11.42firenze.it>
2024-06-13 18:58:20 +0200

    test

--- headers/lexer.h
+++ headers/lexer.h
@@ -6,7 +6,7 @@
 /*   By: aeid <aeid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 20:50:21 by aeid              #+#    #+#             */
-/*   Updated: 2024/06/12 18:57:59 by aeid             ###   ########.fr       */
+/*   Updated: 2024/06/13 18:50:55 by aeid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 
 typedef enum s_types
 {
-	SPECIAL_SQUOTE,
-	SPECIAL_DQUOTE,
+	SPECIAL_SQUOTE, //in parsing check for escape chars
+	SPECIAL_DQUOTE, //in parsing check for escape chars
 	META, // check for $ | > < >> <<
 	WORD,
 	WORD_WITH_DQUOTE_INSIDE, //check for $
@@ -37,5 +37,8 @@ int ft_isquote(int c);
 int ft_ismeta(int c);
 void ft_special_token(t_data *data, t_types type);
 void ft_word_token(t_data *data, t_types type);
+void dollar_meta(t_data *data, t_list *node, t_tkn_data *token);
+void pipe_meta(t_data *data, t_list *node, t_tkn_data *token);
+void redirect_meta(t_data *data, t_list *node, t_tkn_data *token);
 
 #endif
