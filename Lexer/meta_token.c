62e36b8879385a84ee096cfcbaaef9a7c2c316b3
Amir Eid <aeid@c2r3p11.42firenze.it>
2024-06-20 17:13:55 +0200

    fix the name of function

--- Lexer/meta_token.c
+++ Lexer/meta_token.c
@@ -6,13 +6,13 @@
 /*   By: aeid <aeid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 20:15:14 by aeid              #+#    #+#             */
-/*   Updated: 2024/06/14 18:59:02 by aeid             ###   ########.fr       */
+/*   Updated: 2024/06/20 17:13:11 by aeid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../headers/minishell.h" 
 
-static void dollar_token_handler(t_data *data, t_list *node, t_tkn_data *token)
+static void meta_token_handler(t_data *data, t_list *node, t_tkn_data *token)
 {
 	if (data->args[data->current] == '$')
 		dollar_meta(data, node, token);
@@ -30,7 +30,7 @@ void ft_meta_token(t_data *data, t_types type)
 	memory_allocator((void **)&node, sizeof(t_list));
 	memory_allocator((void **)&token, sizeof(t_tkn_data));
 	token->type = type;
-	dollar_token_handler(data, node, token);
+	meta_token_handler(data, node, token);
 	(data->current)--;
 }
 
